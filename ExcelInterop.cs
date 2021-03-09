811f8ad7c66525f75cfe999e92c87a52deb8d1d9
giuliohome <giuliohome@gmail.com>
2021-03-09 14:54:44 +0100

    try finally to quit excel

--- ExcelInterop.cs
+++ ExcelInterop.cs
@@ -5,25 +5,35 @@ namespace SortXlsxLib
 {
     public static class ExcelInterop
     {
-        public static void SortTable(string path, string sheet,
+        public static void SortTable(string path, string sheet, bool visible,
             // start of data, header excluded, e.g row 2 if header is in row 1
             int start_row, int start_col, int end_row, int end_col,
             int sort_col)
         {
             var excelApp = new Application();
-            excelApp.Visible = false;
+            excelApp.Visible = visible;
 
             Workbook wb = excelApp.Workbooks.Open(path);
 
-            Worksheet ws = (Worksheet)wb.Worksheets[sheet];
+            try
+            {
+                Worksheet ws = (Worksheet)wb.Worksheets[sheet];
+                ws.Activate();
 
-            var tab = (Range)ws.Range[ws.Cells[start_row, start_col], ws.Cells[end_row, end_col]];
+                var tab = (Range)ws.Range[ws.Cells[start_row, start_col], ws.Cells[end_row, end_col]];
 
-            tab.Sort(ws.Cells[start_row, sort_col], XlSortOrder.xlAscending);
+                tab.Sort(ws.Cells[start_row, sort_col], XlSortOrder.xlAscending);
 
-            wb.Save();
-            wb.Close();
-            excelApp.Quit();
+                wb.Save();
+            }
+            finally
+            {
+                wb.Close();
+                excelApp.Quit();
+            }
+
+            
+            
         }
     }
 }
